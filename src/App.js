eb488529480eb7f2a9fd5236a3c2e32a15a72fcf
taniebrand <mat@taniebrand.com>
2023-09-13 20:01:37 -0300

    Add NavigationBar and HeroSlider components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './NavigationBar';
+import HeroSlider from './HeroSlider';
 
 function App() {
   return (
     <div className="App">
       <div className="content">
+        <NavigationBar />
+        <HeroSlider />
         <h1>Content Goes here</h1>
       </div>
     </div>
---
+++ src/HeroSlider.js
@@ -0,0 +1,46 @@
+import Carousel from 'react-bootstrap/Carousel';
+import slide1 from './img/hero-image-2.jpg'
+
+const HeroSlider = () => {
+    return ( 
+        <Carousel>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+     );
+}
+ 
+export default HeroSlider;
